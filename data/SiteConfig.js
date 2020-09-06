da302f10b6e622e92af2cee56995974a09b01940
Cristhian García Vélez <escipion25@gmail.com>
2020-09-06 18:19:30 -0500

    Configura Netlify Cms

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ const config = {
   siteTitleShort: "Sudo Build Dev", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Sudo Build Dev", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://sudobuilddev.netlify.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://sudobuild.dev", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Otra comunidad geek.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
